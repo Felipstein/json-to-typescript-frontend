f81fcbedcf76e223f654b9e84880a631ebf7e888
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-22 10:59:57 -0300

    ✨ feat: Add api service

---
+++ src/services/api.service.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance } from 'axios';
+
+export class APIService {
+
+  private readonly api: AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: 'http://localhost:3333/v1',
+    });
+  }
+
+  async transpileCode(code: string): Promise<string> {
+    const response = await this.api.post('/transpile', { json: code });
+
+    console.log(response);
+
+    return `${code}\ntest`;
+  }
+
+}
