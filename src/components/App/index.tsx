0c1e34ef369539fb78c49fc7aba3508c50f93324
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-21 22:41:28 -0300

    ✨ feat: Add footer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -15,12 +15,34 @@ export const App: React.FC = () => {
     <ThemeProvider theme={_dark}>
       <GlobalStyles />
       <S.Layout>
-        <Logo />
+        <S.MainContainer>
+          <Logo />
 
-        <Text id='sub-title'>
-          Transforme seu JSON em uma interface TypeScript com facilidade e rapidez!
-        </Text>
+          <Text id='sub-title'>
+            Transforme seu JSON em uma interface TypeScript com facilidade e rapidez!
+          </Text>
 
+        </S.MainContainer>
+
+        <S.Footer>
+          <div className="block-footer by">
+            <Text id='footer-by' size='sm'>
+              by Felipe Oliveira
+            </Text>
+            <Text>
+              https://github.com/Felipstein
+            </Text>
+          </div>
+
+          <div className="block-footer">
+            <Text>
+              Icon 1
+            </Text>
+            <Text>
+              Icon 2
+            </Text>
+          </div>
+        </S.Footer>
       </S.Layout>
     </ThemeProvider>
   );
--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  min-height: 100vh;
+`;
+
+export const MainContainer = styled.main`
   width: 90vw;
 
   display: flex;
@@ -9,7 +16,28 @@ export const Layout = styled.div`
 
   margin: 4.8rem auto;
 
+  flex: 1;
+
   #sub-title {
     margin-top: 3.2rem;
+  }
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.6rem;
+
+  padding: 3.4rem;
+
+  .block-footer {
+    width: 30rem;
+
+    &.by {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
   }
 `;
